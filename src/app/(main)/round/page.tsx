a666de4f4ff5b6b693a9a8b94b2a0b2b50e09f61
Avinnn <anjasmoonton@gmail.com>
2025-05-28 21:40:41 +0700

    creating round page

---
+++ src/app/(main)/round/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import Rounds from '@/src/components/organisms/roundPage';
+import { SessionProvider } from 'next-auth/react';
+
+export default function RoundPage() {
+  return (
+    <SessionProvider>
+      <Rounds />
+    </SessionProvider>
+  );
+}
