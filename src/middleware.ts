c53359bd37f118a363cff725fa251380445fe746
Avinnn <anjasmoonton@gmail.com>
2025-06-08 22:56:28 +0700

    change dir of middleware

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+  const { pathname } = req.nextUrl;
+
+  // Redirect guest (unauthenticated) from /admin to /login
+  if (!token && pathname.startsWith('/admin')) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  // Block judges and guest from accessing /admin
+  if (pathname.startsWith('/admin') && token?.role !== 'ADMIN') {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  // Block guest and admin from /round
+  if (pathname.startsWith('/round') && token?.role !== 'JUDGES') {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/admin/:path*', '/round/:path*'],
+};
