f3ec99986c30f88cf5e60158d840a63c8468de59
Avinnn <anjasmoonton@gmail.com>
2025-06-28 19:35:54 +0700

    fix error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: ['error'], // opsional: hapus atau ubah sesuai kebutuhan
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
